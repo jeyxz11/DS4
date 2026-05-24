c4104312fa13923f530adf9c1d9bd1986109b554
Alan Bejarano <alanhoracio2112@gmail.com>
2026-05-24 10:44:28 -0700

    Eliminando el navegador de 300MB

---
+++ game_logic/app.py
@@ -0,0 +1,25 @@
+"""
+Tic Tac Toe Game
+Author: Federico Cirett Galan
+"""
+#from game_logic import game
+from game_logic import play_game
+from menu import display_menu
+
+def main()-> None:
+    """ Main function to run the Tic Tac Toe game
+    """
+    while True:
+        choice = display_menu()
+        if choice == 1:
+            play_game(1) # play vs computer
+        elif choice == 2:
+            play_game(2) # two players
+        elif choice == 3:
+            print("Exiting the game. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+if __name__ == "__main__":
+    main()
